272984f1b73fd98604d5d32ab297d4e38fdc4a7e
GeorgeGiak1 <gg923@ic.ac.uk>
2024-08-22 15:27:57 +0300

    Finished landing page
    Does not include links to whatsapp group

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,8 +1,10 @@
 import { tutors } from "../data/tutorInfo.js";
-import { servicesOffered } from "../data/servicesOfferedInfo.js";
+import { servicesOffered, theDifferences } from "../data/servicesOfferedInfo.js";
+import { resultsInfo } from "../data/resultsInfo.js";
 
 const numberOfCards = 3;
-let tutorCardHTML = ''
+let tutorCardHTML = '';
+let resultsCardHTML = '';
 
 for (let i = 0; i < numberOfCards; i++) {
   const { picture, name, description, uni } = tutors[i];
@@ -24,8 +26,24 @@ for (let i = 0; i < numberOfCards; i++) {
   `
 } 
 
+for (let i = 0; i < numberOfCards; i++) {
+  const { name, description } = resultsInfo[i];
+
+  resultsCardHTML += `
+    <div class="general-card results-card">
+      <div class="quote-mark-container">
+        <img class="quote-mark" src="images/icons/quote-left-icon.svg">
+      </div>
+      <div class="results-description">${description}</div>
+      <div class="results-name">${name}</div>
+    </div>
+  `
+}
+
 document.querySelector('.js-tutor-card-container')
   .innerHTML = tutorCardHTML;
+document.querySelector('.js-result-card-container')
+  .innerHTML = resultsCardHTML;
 
 function changeImageTextElement() {
   let currentValue;
@@ -41,20 +59,20 @@ function changeImageTextElement() {
       matchingValue = value;
   })
 
-  renderImageTextElement(true, matchingValue, 'What we offer', '.js-services-offered-container')
+  renderImageTextElement(true, matchingValue, 'What we offer', '.js-services-offered-container', 'grey', 0)
 }
 
-function renderImageTextElement(withOptions, value, title, classTag) {
+function renderImageTextElement(withOptions, value, title, classTag, backgroundColor, order) {
   let textImageElementHTML = '';
   
 
   textImageElementHTML += `
     <div class="element-title">${title}</div>
     <div class="picture-text-container">
-      <div class="element-image-container">
+      <div class="element-image-container" style="order: ${order}">
         <img class="element-image" src=${value.image}>
       </div>
-      <div class="text-container">
+      <div class="${backgroundColor}-text-container">
         <div class="text-header">
           ${value.title}
         </div>
@@ -80,11 +98,11 @@ function addEventListenersToRadio() {
   document.getElementsByName('first-radio').forEach(value => {
     value.addEventListener('click', () => {
       changeImageTextElement();
-      console.log('I am listening')
     });
   });
 }
 
-renderImageTextElement(true, servicesOffered[0], 'What we offer', '.js-services-offered-container');
+renderImageTextElement(true, servicesOffered[0], 'What we offer', '.js-services-offered-container', 'grey', 0);
 addEventListenersToRadio();
+renderImageTextElement(false, theDifferences, 'What we do differently', '.js-the-differences-container', 'white', 1);
 
