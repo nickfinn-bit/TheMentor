e0634a08fb861e17b421d4b421d7a5c45dd9358e
nickfinn-bit <nicholas.finnemore@gmail.com>
2024-09-05 22:22:22 +0800

    Update tutorInfo.js

--- data/tutorInfo.js
+++ data/tutorInfo.js
@@ -5,15 +5,15 @@ const tutors = [{
   &#x2022; 4 A&#42; achieved in A levels <br>
   &#x2022; Dean's list Imperial <br>
   &#x2022; Kangaroo Math Challenge`,
-  uni: 'images/uni-images/imperial-logo.png'
+  uni: 'images/uni-images/MIT-logo.png'
 }, {
   picture: 'images/tutor-images/tutor-picture-nick.png',
   name: 'Nick',
   description: `
-  &#x2022; 4 A&#42; achieved in A levels <br>
-  &#x2022; Dean's list MIT <br>
+  &#x2022; First Class <br>
+  &#x2022; Highest Maths Scorer in Cohort <br>
   &#x2022; Kangaroo Math Challenge`,
-  uni: 'images/uni-images/MIT-Logo.png'
+  uni: 'images/uni-images/imperial-Logo.png'
 }, {
   picture: 'images/tutor-images/tutor-picture-example-2.jpg',
   name: 'Will',
