80286cf790fc0468e9973207ea8d5ec0968519e1
nickfinn-bit <nicholas.finnemore@gmail.com>
2024-09-05 22:22:44 +0800

    Update tutorInfo.js

--- data/tutorInfo.js
+++ data/tutorInfo.js
@@ -12,7 +12,7 @@ const tutors = [{
   description: `
   &#x2022; First Class <br>
   &#x2022; Highest Maths Scorer in Cohort <br>
-  &#x2022; Kangaroo Math Challenge`,
+  &#x2022; Yale Scholar's Cup Finalist`,
   uni: 'images/uni-images/imperial-Logo.png'
 }, {
   picture: 'images/tutor-images/tutor-picture-example-2.jpg',
