7b553ed4275bb0a347ac27229936368ab9ff9925
nickfinn-bit <nicholas.finnemore@gmail.com>
2024-09-06 19:26:24 +0800

    Update resultsInfo.js

--- data/resultsInfo.js
+++ data/resultsInfo.js
@@ -1,10 +1,10 @@
 const resultsInfo = [{
-  name: 'Jessica',
+  name: 'Jessica, HK',
   description: `My son went from a D in Maths to an A in just one year! I could not recommend this service more. The tutors are knowledgable both in their fields and in the exam technique meaning they prepare your child for the exam perfectly.`
 }, {
-  name: 'Stuart',
+  name: 'Stuart, China',
   description: `A 5 star service. Reliable, easy and most of all effective. I will definitely use this with my second daugher.`
 }, {
-  name: 'Katrina',
+  name: 'Katrina, London',
   description: `Very friendly and knowledgable tutors. I would recommend the service to anyone unsure and specifically say that Marie was brilliant in helping my daughter get into Oxford.`
 }]
