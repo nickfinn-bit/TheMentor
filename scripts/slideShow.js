7699e32d5994af444e866ac914305c95e254383e
George <george.dsig@gmail.com>
2024-09-07 14:17:53 +0300

    Make Slideshow restart from the correct point

--- scripts/slideShow.js
+++ scripts/slideShow.js
@@ -1,5 +1,6 @@
 let counter = 1;
-setInterval(() => {
+
+let slideshowIntervalId = setInterval(() => {
   document.getElementById('radio' + counter).checked = true;
   counter++;
   if (counter > 3) {
@@ -7,3 +8,20 @@ setInterval(() => {
   }
 }, 15000);
 
+document.querySelectorAll('.manual-btn')
+  .forEach(value => {
+    value.addEventListener('click', () => {
+      clearInterval(slideshowIntervalId);
+      const radioReferance = value.htmlFor;
+      const buttonClicked = radioReferance.slice(-1);
+      counter = (buttonClicked == 3) ? 1 : Number(buttonClicked) + 1;
+
+      slideshowIntervalId = setInterval(() => {
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if (counter > 3) {
+          counter = 1;
+        };
+      }, 15000);
+    });
+  });
