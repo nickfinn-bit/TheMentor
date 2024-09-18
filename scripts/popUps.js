c521f4571d2606b3ed982f283abe2a625b432a9d
George <george.dsig@gmail.com>
2024-09-18 11:09:08 +0100

    Add second popup

--- scripts/popUps.js
+++ scripts/popUps.js
@@ -1,8 +1,12 @@
 contactUsButton = document.querySelector('.contact-us-container');
 interestedButton = document.querySelector('.find-tutor-button');
+applyButton = document.querySelector('.footer-button')
 
-interestedPopUp = document.querySelector('.interested-popup')
-closeInterestedPopUp = document.querySelector('.interested-popup .close-popup')
+interestedPopUp = document.querySelector('.interested-popup');
+applyPopUp = document.querySelector('.apply-popup');
+
+closeInterestedPopUp = document.querySelector('.interested-popup .close-popup');
+closeApplyPopUp = document.querySelector('.apply-popup .close-popup')
 
 buttonsList = [contactUsButton, interestedButton];
 
@@ -11,7 +15,29 @@ buttonsList.forEach(button => {
     interestedPopUp.classList.remove('hidden')
   })
 })
+applyButton.addEventListener('click', () => {
+  applyPopUp.classList.remove('hidden');
+})
 
 closeInterestedPopUp.addEventListener('click', () => {
   interestedPopUp.classList.add('hidden');
 })
+closeApplyPopUp.addEventListener('click', () => {
+  applyPopUp.classList.add('hidden');
+})
+
+/*
+function closePopUpAuto(popUpClass) {
+  document.querySelectorAll(`main *:not(${popUpClass}):not(.contact-us-container):not(.find-tutor-button):not(.footer-button)`)
+    .forEach(element => {
+      element.addEventListener('click', () => {
+        document.querySelector(popUpClass).classList.add('hidden');
+        console.log(document.querySelector(popUpClass).classList)
+        console.log(element);
+      })
+    })
+}
+
+closePopUpAuto('.apply-popup');
+closePopUpAuto('.interested-popup');
+*/
