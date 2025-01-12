c2ba3c5bf8fe8820c52111a77e3d4c5c13f18e8e
nickfinn-bit <nicholas.finnemore@gmail.com>
2025-01-12 19:33:30 +0000

    scaling

--- scripts/popUps.js
+++ scripts/popUps.js
@@ -2,13 +2,13 @@ fadeOutElem = document.querySelector('.fade-out');
 
 contactUsButton = document.querySelector('.contact-us-container');
 interestedButton = document.querySelector('.find-tutor-button');
-applyButton = document.querySelector('.footer-button')
+// applyButton = document.querySelector('.footer-button')
 
 interestedPopUp = document.querySelector('.interested-popup');
-applyPopUp = document.querySelector('.apply-popup');
+// applyPopUp = document.querySelector('.apply-popup');
 
 closeInterestedPopUp = document.querySelector('.interested-popup .close-popup');
-closeApplyPopUp = document.querySelector('.apply-popup .close-popup');
+// closeApplyPopUp = document.querySelector('.apply-popup .close-popup');
 
 buttonsList = [contactUsButton, interestedButton];
 
@@ -18,19 +18,19 @@ buttonsList.forEach(button => {
     fadeOutElem.classList.remove('hidden');
   })
 })
-applyButton.addEventListener('click', () => {
-  applyPopUp.classList.remove('hidden');
-  fadeOutElem.classList.remove('hidden');
-})
+// applyButton.addEventListener('click', () => {
+//   applyPopUp.classList.remove('hidden');
+//   fadeOutElem.classList.remove('hidden');
+// })
 
 closeInterestedPopUp.addEventListener('click', () => {
   interestedPopUp.classList.add('hidden');
   fadeOutElem.classList.add('hidden');
 })
-closeApplyPopUp.addEventListener('click', () => {
-  applyPopUp.classList.add('hidden');
-  fadeOutElem.classList.add('hidden');
-})
+// closeApplyPopUp.addEventListener('click', () => {
+//   applyPopUp.classList.add('hidden');
+//   fadeOutElem.classList.add('hidden');
+// })
 
 /*
 function closePopUpAuto(popUpClass) {
