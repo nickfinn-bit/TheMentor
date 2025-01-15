ba94d5ab6d8badaf357cc7e1ab41c8fc4530ee56
nickfinn-bit <nicholas.finnemore@gmail.com>
2025-01-15 15:19:16 +0000

    Update graph.js

--- scripts/graph.js
+++ scripts/graph.js
@@ -6,7 +6,7 @@ new Chart(chartElement, {
     labels: [['Our', 'Students (%)'], ['The', 'Average (%)']],
     datasets: [{
       label: 'Students getting into target uni',
-      data: [75, 17],
+      data: [55, 17],
       backgroundColor: ['#7E1633', 'rgb(200, 200, 200)'],
       barThickness: 76,
     }]
